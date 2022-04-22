1f9953e3b605133065fa7832971efb37dd94a5d7
Patrick Hart <pathart612@gmail.com>
2022-04-21 20:55:31 -0400

    commit tree 6d37ec7c34c60034b600c1c128bc68b29f79606d

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverse = ""
+  for(let i = word.length - 1; i >= 0; i --){
+    reverse += word[i]
+  }
+  if (word === reverse){
+    return true
+  } else{
+    return false
+  }
 }
 
 /* 
